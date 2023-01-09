2cbb36d0972d72cea9c1a5604f2c2d5f9d0f14ec
maminyi <minyi.ma@mail.utoronto.ca>
2023-01-09 16:25:56 -0500

    Finished scraping, added all generated datasets

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 # import requests
-# import csv
+import csv
 
 URL = "https://stackoverflow.com/questions?"  # Default URL to scrape questions
 TAGGED_URL = "https://stackoverflow.com/questions/tagged/"
-Q_URL = "https://stackoverflow.com"  # BASE URL to get question detail.
-# build = "https://stackoverflow.com/questions/tagged/ui-automation?tab=frequent&page=1&pagesize=15"
-# Using 50 question per page & limit rate to 3 page for rate limit consideration.
-PAGE_LIMIT = 3
-CURR_PAGE_LIMIT = 3
+Q_URL = "https://stackoverflow.com"  # Base URL to get question detail.
+# Using 50 question per page & limit rate to 3 page for daily rate limit consideration.
+PAGE_LIMIT = 2
+CURR_PAGE_LIMIT = 2
 
 
 def build_url(base_url=URL, tab="Frequent", page=1, tag="", question_href=""):
@@ -19,29 +18,9 @@ def build_url(base_url=URL, tab="Frequent", page=1, tag="", question_href=""):
     if question_href:
         return Q_URL + question_href
     # Default using 50 post per page here to calculate max page.
-    return f"{base_url}?tab={tab}&page={page}&pagesize=15"
+    # Using 50 here because that's how many get scraped no matter what pagesize is specified in link.
+    return f"{base_url}?tab={tab}&page={page}&pagesize=50"
 
-
-# # Get page and read HTML from the page
-# update_URL = build_url(tag="ui-automation")
-# print(update_URL)
-# response = urlopen(update_URL)
-# html = response.read()
-# soup = BeautifulSoup(html)
-
-# # Get maximum number of page under the tag to update number of page to be scraped.
-# max_page = int(soup.find_all(
-#     "a", class_="s-pagination--item js-pagination-item")[-2].text)
-# CURR_PAGE_LIMIT = min(PAGE_LIMIT, max_page)
-
-
-# questions_list = soup.find_all("h3", class_="s-post-summary--content-title")
-
-# new_URL = build_url(question_href=questions_list[0].a['href'])
-# print(new_URL)
-
-# for q in questions_list:
-#     print(q.a['href'])
 
 def scrape_question(href):
     """
@@ -52,15 +31,22 @@ def scrape_question(href):
     q_html = q_response.read()
     q_soup = BeautifulSoup(q_html)
 
-    # One of the answer
-    print(q_soup.find("div", class_="answercell post-layout--right").div.text)
-    # Add detail to question.
+    # Add prpoperties (Title, Description, Vote, Views) of questions.
     question = {}
     question["Title"] = q_soup.find(
         "h1", class_="fs-headline1 ow-break-word mb8 flex--item fl1").text
     question["Description"] = q_soup.find(
         "div", class_="s-prose js-post-body").text
-    print(question)
+    question["Views"] = q_soup.select_one("div[title*=Viewed]").text.split()[1]
+    question["Votes"] = q_soup.find(
+        "div", class_="js-vote-count flex--item d-flex fd-column ai-center fc-black-500 fs-title").text
+
+    answers = q_soup.find_all("div", class_="answercell post-layout--right")
+    for i in range(len(answers)):
+        answers[i] = answers[i].div.text
+    question["Answers"] = answers
+
+    return question
 
 
 def scrape_page(page=1, tag=""):
@@ -76,32 +62,23 @@ def scrape_page(page=1, tag=""):
     if page == 1:
         global CURR_PAGE_LIMIT
         # Get maximum number of page under the tag to update number of page to be scraped.
-        max_page = int(soup.find_all(
-            "a", class_="s-pagination--item js-pagination-item")[-2].text)
+        page_numbers = soup.find_all(
+            "a", class_="s-pagination--item js-pagination-item")
+        if len(page_numbers) >= 2:
+            max_page = int(page_numbers[-2].text)
+        else:
+            max_page = 1
         CURR_PAGE_LIMIT = min(PAGE_LIMIT, max_page)
 
     questions_list = soup.find_all(
         "h3", class_="s-post-summary--content-title")
 
     page_questions = []
-    # for q in questions_list:
-
-    # For each question, get url to the question and scrape the actual description and answer
-    question = scrape_question(href=questions_list[0].a['href'])
-
-    # for summary in question_summary:
-    #     question = summary.find(class_="question-hyperlink").text
-    #     vote_count = summary.find(class_="vote-count-post").find("strong").text
-    #     answers_count = summary.find(class_="status").find("strong").text
-    #     view_count = summary.find(class_="views").text.split()[0]
-    #     page_questions.append({
-    #         "question":  question,
-    #         "answers": answers_count,
-    #         "views": view_count,
-    #         "votes": vote_count
-    #     })
-
-    # return page_questions
+    for q in questions_list:
+        # For each question, get url to the question and scrape the actual description and answer
+        question = scrape_question(href=q.a['href'])
+        page_questions.append(question)
+    return page_questions
 
 
 def scrape(tag=""):
@@ -109,26 +86,28 @@ def scrape(tag=""):
     Function to scrape to PAGE_LIMIT
     """
     questions = []
-    # for i in range(1, CURR_PAGE_LIMIT + 1):
-    scrape_page(1, tag)
-    # page_questions = scrape_page(i)
-    # questions.extend(page_questions)
+    for i in range(1, CURR_PAGE_LIMIT + 1):
+        questions.extend(scrape_page(1, tag))
     return questions
 
 
-scrape("ui-automation")
-# # def export_data():
-# #     data = scrape()
-# #     with open("questions.csv", "w") as data_file:
-# #         fieldnames = ["answers", "question", "views", "votes"]
-# #         data_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
-# #         data_writer.writeheader()
-# #         for d in data:
-# #             data_writer.writerow(d)
-# #         print("Done")
+def export_data(tag=""):
+    data = scrape(tag)
+    if tag:
+        filename = tag + ".csv"
+    else:
+        filename = "question.csv"
+    with open(filename, "w", newline="") as data_file:
+        fieldnames = ["Title", "Description", "Views", "Votes", "Answers"]
+        data_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+        data_writer.writeheader()
+        for d in data:
+            data_writer.writerow(d)
+    print("Export tag: " + tag + " finished")
 
 
-# if __name__ == "__main__":
-#     from pprint import pprint
-#     pprint(scrape())
-# #     # export_data()
+if __name__ == "__main__":
+    # Scraping Stackoverflow using existing tags related to web automation, ui automation, data scraping etc.
+    tags = ["ui-automation", "webautomation", "web-scraping"]
+    for t in tags:
+        export_data(t)
